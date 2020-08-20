91541fab8a1ff7f8b7f246da69ce45cf247f41e9
vvmteam <66971940+vvmteam@users.noreply.github.com>
2020-08-20 21:18:08 +0700

    Update

---
+++ about.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "./inc/headtag.php" ?>
+
+<body>
+  <?php include "./inc/header.php" ?>
+
+  <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url('images/vvm.jpg');"
+    data-aos="fade" data-stellar-background-ratio="0.5" data-aos="fade">
+    <div class="container">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-md-7 text-center" data-aos="fade-up" data-aos-delay="400">
+  
+          <h1 class="text-white">V.V.M Team</h1>
+          <p>Những con người đồng điệu tụ họp lại với nhau làm nên V.V.M Team.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="site-section">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-5 ml-auto order-lg-2 mb-5" data-aos="fade-up" data-aos-delay="200">
+          <img src="images/vvm2.jpg" alt="Image" class="img-fluid">
+        </div>
+        <div class="col-lg-6 order-lg-1" data-aos="fade-up" data-aos-delay="100">
+          <div class="site-section-heading text-left mb-5 w-border">
+            <h2>Về V.V.M Team và M.T Game</h2>
+          </div>
+		  <p class="lead">Nhờ những người đặc biệt, V.V.M Team đã ra đời.</p>
+          <p>Được thành lập ngày 11/4/2014, từ 1 thành viên duy nhất cùng cái tên nguyên bản VietVocaloid Miro-chan với định hướng ban đầu là cover những bài hát Vocaloid sang tiếng Việt, V.V.M Team đã phát triển thành một nhóm nhỏ với 8 thành viên hoạt động chủ yếu ở 2 lĩnh vực: Cover Vocaloid và Việt hóa Game. Nhóm hoạt động dựa trên sở thích cũng như đam mê, nên sẽ luôn bốc cháy hết mình với các dự án.</p>
+          <p>Vốn dĩ ý định tham gia vào việc Việt hóa Game không xuất hiện ngay từ khi thành lập nhóm. Cuối năm 2017, Miro đột nhiên có ý tưởng: “Nếu như game đó không có Việt hóa thì chỉ cần tự Việt hóa để tự chơi là được!”. M.T Game đã được thành lập từ đó, cùng với lần đầu ra mắt bản Việt hóa Detention. Mục đích ra đời của M.T Game đơn giản chỉ để mọi người phân biệt giữa những bài hát được upload trên channel chính của nhóm cũng như những dự án Việt hóa game, tránh gây nhầm lẫn.</p>
+        </div>
+  
+      </div>
+    </div>
+  </div>
+
+    <div class="site-section">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
+            <h2 class="mb-5">Thành viên của V.V.M Team</h2>
+            <p>Những con người nhỏ bé nhưng mang niềm đam mê mãnh liệt</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/miro.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Mèo <b>Miro</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Trưởng nhóm</span>
+                <p class="mb-4">Trưởng nhóm siêu lười biếng của V.V.M Team. Là người thực hiện toàn bộ các dự án trên V.V.M Team.</p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/rinta.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Sói <b>RinTa</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Thư ký tổng hợp</span>
+                <p class="mb-4">Thành viên đầu tiên của V.V.M Team, mang vẻ ngoài ngầu lòi, RinTa là một nhân viên siêu đa năng của nhóm.</p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/Quynh.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Chim <b>Quỳnh</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Lyricist</span>
+                <p class="mb-4">Cô gái đa năng sinh cùng năm với Rinta.  Tính tình trầm lặng nhưng cũng vô cùng cá tính.</p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/Ca.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Thỏ <b>Cá</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Lyricist</span>
+                <p class="mb-4">Chị cả của gia đình V.V.M Team, là một người sống vô cùng tình cảm cũng như luôn năng động và dễ thương.</p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/yui.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Sóc Mèo <b>Yui</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Youtaite</span>
+                <p class="mb-4">Một cô bé lấy việc trốn deadline làm niềm đam mê mãnh liệt.</p>
+              </div>
+
+            </div>
+          </div>
+
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/aiko.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Thỏ <b>Aiko</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Translator</span>
+                <p class="mb-4">Cô bé đầy cá tính, từ một fan lâu năm của V.V.M Team trở thành một thành viên chính thức của nhóm.</p>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/vue.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Hồ Ly <b>Vuệ</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Translator</span>
+                <p class="mb-4">Cô gái lớn tuổi nhất của V.V.M Team, đầy bí ẩn cũng như vô cùng tài giỏi khi có thể đọc được nhiều ngôn ngữ.</p>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/Asami.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Bướm đêm <b>Asami</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Lyricist</span>
+                <p class="mb-4">Cô bé trầm tính nhất gia đình V.V.M Team, dù không có nhiều thời gian nhưng Asami vẫn luôn cố gắng hết mình.</p>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/cloud.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Gấu Trúc đỏ <b>_Cloud_</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Kỹ thuật viên</span>
+                <p class="mb-4">Là thành viên nam hiếm hoi của nhóm chỉ đứng sau Miro, Cloud rất giỏi trong việc vọc vạch và nghiên cứu.</p>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/ken.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Cáo <b>Kén</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Illustrator</span>
+                <p class="mb-4">Một cô bé năng động cũng như đầy nhiệt huyết với công việc, Kén luôn cố gắng hoàn thành công việc trước deadline.</p>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/haruka.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Sư tử <b>Haruka</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Lyricist</span>
+                <p class="mb-4">Cô bé trầm tính, vừa tham gia V.V.M Team. Haruka sẽ sớm ra mắt với các bạn fan yêu mến của V.V.M Team ngay thôi!</p>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img src="images/member/na.jpg" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">Bò tót <b>Nả</b></h2>
+                <span class="d-block mb-2 text-white-opacity-05">Illustrator</span>
+                <p class="mb-4">Cô gái mang đầy tâm tư trong lòng mình cùng nét vẽ. Chúng ta sẽ sớm được xem những sản phẩm đầy nội tâm của Nả.</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  
+
+
+  <?php include "./inc/footer.php" ?>
+
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/mediaelement-and-player.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
---
+++ donate.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "./inc/headtag.php" ?>
+  <body>
+  <?php include "./inc/header.php" ?>
+    
+  
+ 
+
+  <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url('images/vvm.jpg');"
+    data-aos="fade" data-stellar-background-ratio="0.5" data-aos="fade">
+    <div class="container">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-md-7 text-center" data-aos="fade-up" data-aos-delay="400">
+          <h1 class="text-white">Donation</h1>
+          <p>Thông tin Donate của V.V.M Team</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class="site-section">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
+            <h2 class="mb-5">Danh sách những bạn đã Donate cho V.V.M Team</h2>
+            <p>Nguyện ý nhỏ nhưng mang lại động lực vô cùng lớn</p>
+          </div>
+
+        </div>
+ <div class="row">
+<p>Nguyễn Minh Tâm, Đinh Công Nhựt, Nguyễn Việt Đức, Rika (Big Donater), Trần Hồ Hoàng Tấn, Phú Quốc Trung, bạn có nick name Thiện Nguyễn trên Youtube, Tomokazu Iwasaki, Trịnh Công Trường, Đỗ Nghi (Kén), Đặng Phước Giàu (Rich)
+</p>
+</div>
+      </div>
+    </div>
+
+    <div class="site-section bg-dark block-13">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="site-section-heading text-center mb-5 w-border col-md-6 mx-auto">
+            <h2 class="mb-5">Thông tin Donate của V.V.M Team</h2>
+          </div>
+        </div>
+ <p>
+ <b>Ví Momo</b>: 0399260137<br>
+ <b>Paypal</b>: miro.vvmteam@gmail.com<br>
+ <b>Chuyển khoản</b>:<br>
+- Số tài khoản: 193685595<br>
+- Số thẻ: 9704321143845667<br>
+- Tên chủ tài khoản: TRUONG BINH LONG<br>
+- Tên ngân hàng: NH Viet Nam Thinh Vuong (VPBank)<br>
+- Chi nhánh: TP.HCM<br>
+- Tỉnh/Thành phố: TP.HCM<br>
+ </p>
+      </div>
+    </div>
+
+
+
+  <?php include "./inc/footer.php" ?>
+
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/mediaelement-and-player.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+
+
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
