f222ffbccc6b641b8770f0e89523b034369172cb
V.V.M Team <miro.vvmteam@gmail.com>
2025-12-19 06:59:53 +0700

    Initial commit

---
+++ old/inc/memories.php
@@ -0,0 +1,127 @@
+<?php
+$txt['n1'] = 'Yoonni';
+$txt['s1'] = 'Mỗi một MV là một kỉ niệm luôn ấy :3 Mấy cậu tuyệt lắmmmm!';
+
+$txt['n2'] = 'Dê';
+$txt['s2'] = 'Hồi ấy mình thích team lắm, thích nhất là Quỳnh. Có vẽ tặng sinh nhật Quỳnh nữa, nhưng hình như lần đó Quỳnh hông thấy... Bây giờ team vẫn là see first của mình, là kỉ niệm tuổi thơ của mình.';
+
+$txt['n3'] = 'Envy';
+$txt['s3'] = 'Lần đầu tiên được tham gia vào nhóm chat, được nói chuyện cùng mọi người rất vui. Đặc biệt là admin Miro lầy lắm luôn á nhưng mà lại là người dễ thương nhất Ù w Ú';
+
+$txt['n4'] = 'Touki';
+$txt['s4'] = 'Nhớ những ngày hóng video mới của team...';
+
+$txt['n5'] = 'Tokana';
+$txt['s5'] = 'Hôm đó đang nghe nhạc của team thì em gái tớ đạp cửa vô phòng, thấy mình xem nó tò mò tìm xem thử, ai ngờ nghiện luôn. Mỗi lần nhóm có bài mới em tớ thường sang phòng cùng xem với tớ. Chúng tớ đã có vô số kỷ niệm cùng với các bài hát của team.';
+
+$txt['n6'] = 'Icyyy Ice';
+$txt['s6'] = 'V.V.M Team là một phần của tuổi thơ mình ah :3 và đây cũng là nơi mà lần đầu tiên mình biết đến ~vocaloid~ (nghiện từ hồi đấy đến giờ luôn ah OwO)';
+
+$txt['n7'] = 'Như';
+$txt['s7'] = 'Các bài hát mà team dịch em luôn cảm thấy rất sát nghĩa gốc và luôn cho em thấy những lời khuyên hữu ích';
+
+$txt['n8'] = 'Raineru';
+$txt['s8'] = 'Mỗi lần lắng nghe bài hát Cánh hạc bốn mùa do V.V.M Team cover, mình đều rưng rưng nước mắt ( ；´Д｀). Không những chỉnh giọng tốt mà lời Việt cũng rất cảm động nữa (´；ω；｀)';
+
+$txt['n9'] = 'Miyamoto Azami';
+$txt['s9'] = 'Hổng biết có gọi là kỉ niệm hong, nhưng khi mà biết đến team thì có một lần mình được giới thiệu với bạn kia, thì bạn ấy khá khó gần nè nhưng mà sau khi cố làm quen và biết bạn ấy thích nhạc ở V.V.M team thì chúng mình đã kết bạn và nghe nhạc, chờ bài mới và cùng hát mấy bài đó, tình bạn chúng mình vẫn rất tốt! Cảm ơn team nhé!';
+
+$txt['n10'] = 'Machi';
+$txt['s10'] = 'Vậy, tớ đã nghe nhạc của team cậu suốt cả thời gian bị cô lập thì đó có phải là kỷ niệm không ha <br>\'-\' nó không vui vẻ gì nhưng tớ nhớ mãi';
+
+$txt['n11'] = 'Miru';
+$txt['s11'] = 'Lúc đang gặp rắc rối về vấn đề học tập cũng như gia đình, xã hội. V.V.M team ra mắt bài Blessing. Ca từ trong bài team làm rất hay em thích lắm, nhất là mấy câu đầu tiên. Nghe xong cảm thấy em hạnh phúc lắm, giải tỏa 1 phần nào đó cảm xúc trong lòng. Thật sự em yêu team lắm. Luôn ủng hộ team.';
+
+$txt['n12'] = 'Tamminh';
+$txt['s12'] = 'Những lúc nghe nhạc do V.V.M Team làm mình cảm thấy rất vui<br>Những lúc buồn phiền cũng nhờ team mà mình vui vẻ trở lại';
+
+$txt['n13'] = 'Nguyễn An Hòa';
+$txt['s13'] = 'Em không có kỉ niệm gì về team ngoại trừ nghe nhạc của team gần như 24/24';
+
+$txt['n14'] = 'Kato Nguyễn';
+$txt['s14'] = 'Mình cũng không biết tả kỉ niệm như thế nào nữa:D, nhưng với mình thật sự có rất nhiều kỉ niệm từ khi biết đến Team và nghe những bài hát mà Team việt hóa<br>Chỉ 1 câu để diễn tả thôi! *thực sự rất hay khi nghe những bài nhạc của Team! Và luôn mãi yêu quý V.V.M Team♡*';
+
+$txt['n15'] = 'Na';
+$txt['s15'] = 'V.V.M Team như 1 chỗ dựa tinh thần của em, mỗi khi buồn em đều nghe những bài hát do team làm và nó làm em vui lên rất nhiều. Em rất yêu quý team và em mong team sẽ phát triển hơn nữa <3';
+
+$txt['n16'] = 'Hạnh Hâm';
+$txt['s16'] = 'V.V.M Team cho mình niềm đam mê với âm nhạc';
+
+$txt['n17'] = 'Yuumie';
+$txt['s17'] = 'Thực sự thì mình cảm thấy dù ra rất ít sản phẩm nhưng mỗi khi ra thì sản phẩm nào cũng rất chất lượng và đầu tư, mình ấn tượng nhất là phần lyrics của nhóm rất hay và bắt tai!';
+
+$txt['n18'] = 'Chiara';
+$txt['s18'] = 'Không hẳn là kỉ niệm, nhưng đối với bản thân mình cũng không biết được nhiều bài hát của Vocaloid, thì mình nhớ những lúc phải cảm thán (và cười lăn 💁‍♀️)  bởi mắm muối mà team hay thêm vào những bài mình biết (như sub không thấy chữ, làm những bài trend như BTBB lmao).';
+
+$txt['n19'] = 'Yina';
+$txt['s19'] = 'Mình ấn tượng với bạn nào đó tên Cá bơi ngửa vì cái tên :33';
+
+$txt['n20'] = 'Nguyệt Nhược Du Yên';
+$txt['s20'] = 'Vào những ngày buồn bã, liền kiếm đến V.V.M Team để nghe những bản nhạc hay, sâu lắng, có cả vui vẻ để vực lại tinh thần.';
+
+$txt['n21'] = 'Ringo';
+$txt['s21'] = 'Mình và người bạn của mình gắn kết lâu hơn nhờ vào những bài hát của team :3';
+
+$txt['n22'] = 'Thanh Nhã';
+$txt['s22'] = 'Mình chả có kỉ niệm gì về team cả nhưng nếu được suy nghĩ ra kỉ niệm thì khoảng thời gian mình xem team là 1 kỉ niệm đẹp chìm đắm trong nhạc';
+
+$txt['n23'] = 'Mạc Nhất Tiêu ';
+$txt['s23'] = 'Đợi :)))';
+
+$txt['n24'] = 'Ocenia';
+$txt['s24'] = 'Những bài hát của nhóm làm ra đôi lúc rất phù hợp tâm trạng của mình. Chẳng hạn như mình rất thích một bài hát buồn nào đó nhưng không hiểu lời bài hát nên khá khó khăn trong việc cảm nhận, nhưng sau khi biết được nhóm cũng có làm bài hát đó ver tiếng Việt thì mình cảm thấy dễ dàng hơn trong việc hiểu rõ lời bài hát, đôi lúc còn cảm thấy những bài hát nhóm làm nghe dễ chịu hơn bản gốc nhiều';
+
+$txt['n25'] = 'HatsuYam';
+$txt['s25'] = 'Một kỉ niệm dài về việc nghe nhạc của nhóm. Nhưng sau này mình có một kỉ niệm cực xấu về Miro, và mình ko thích ổng chút nào. Nhưng dù sao thì mình vẫn thích nghe nhạc của nhóm thôi.';
+
+$txt['n26'] = 'Hao-sama ~Chaos~';
+$txt['s26'] = 'Nghe ngày nghe đêm hư lun cái máy nghe nhạc còn bị phụ huynh mắng nữa chứ =[[';
+
+$txt['n27'] = 'Glitchy';
+$txt['s27'] = 'Hơm biết có được tính không, nhưng có một thời vì một số lí do mà mình không được vui vẻ cho lắm, nên là ngày nào cũng bật bài "Blessing" với cả "Tạm biệt nhé tôi của ngày hôm qua" của team nghe để lấy động lực (´・ω・｀)';
+
+$txt['n28'] = 'Vịt';
+$txt['s28'] = 'Kỉ niệm là lôi kéo con bạn vô V.V.M Team và rồi nó còn nghiện team hơn cả mềnh';
+
+$txt['n29'] = 'Lyn';
+$txt['s29'] = 'Nhờ người bạn ấy mà tớ có những kỷ niệm rất thú vị với âm nhạc của V.V.M Team. Mong mỗi tuần team ra được 1 bài thì càng tốt.Lúc nào cũng chờ team comeback <3';
+
+$txt['n30'] = 'Alicia';
+$txt['s30'] = 'Nhớ có một lần nghe bài nào đấy của team. Nhớ hoài không quên. Cứ hễ mở Youtube lên thì luôn nghe bài đó đầu tiên';
+
+$txt['n31'] = 'Quanhhhh';
+$txt['s31'] = 'Hồi tui mới chia tay người yêu lâu năm nhưng chẳng thể khóc dù bị cắm sừng. Tui đã nghe bài Last Time To Say (không liên quan nhỉ) và khóc một lượt cả đêm... Tui đã rất thoải mái đó. Cám ơn!';
+
+$txt['n32'] = 'Yuuki';
+$txt['s32'] = 'Kỉ niệm thì chắc là suốt ngày ngồi nghe nhạc của nhóm rồi tự nhiên cười như một con điên :v';
+
+$txt['n33'] = 'Cá Biết Bay';
+$txt['s33'] = 'Cái này lâu rồi nên tớ ko nhớ lắm, tớ chỉ nhớ các cậu cùng hát một bài hát kỉ niệm.';
+
+$txt['n34'] = 'Amy';
+$txt['s34'] = 'Bài đầu tiên mà mình nghe là \'Nhảy lên nào\'. Và mình đã bị lôi cuốn ⁽⁽ଘ( ˊᵕˋ )ଓ⁾⁾';
+
+$txt['n35'] = 'Minh';
+$txt['s35'] = 'Thực sự mà nói thì V.V.M Team đã gắn liền với tuổi thơ của tớ từ hồi lớp 7 cho đến bây giờ (lớp 10)';
+
+$txt['n36'] = 'Hasu';
+$txt['s36'] = 'Hát "Giấc mơ xưa và cây đại bạch anh đào" trên sân khấu :")) Cảm giác được giới thiệu cho mọi người về team tự hào lắm ý :>';
+
+$txt['n37'] = 'Tuzki';
+$txt['s37'] = 'Kênh đầu tiên toi đăng ký mà đặt chế độ thông báo 🤣🤣🤣';
+
+$txt['n38'] = 'Min';
+$txt['s38'] = 'Kỉ niệm của mình với team thì chắc là lần đầu nghe nhạc Vocaloid đc việt hoá, lúc đầu mình luôn nghĩ nhạc việt hoá rất tệ, nhưng team đã khiến mình thực sự phải thay đổi cách nghĩ đó và bị ume luôn ❤️❤️❤️';
+
+$txt['n39'] = 'Dương';
+$txt['s39'] = 'Hmm cũng không biết nói thế nào cả. Mình chỉ biết là 1 phần thanh xuân của mình thật đẹp vì được tận hưởng qua các video âm nhạc của các bạn ❤️';
+
+$txt['n40'] = 'Đỗ Cẩn Mai';
+$txt['s40'] = 'Rất nhiều bài hát gắn liền với một phần ký ức đẹp giờ không thể lấy lại';
+
+$txt['n41'] = 'Mây';
+$txt['s41'] = 'Chắc là thời gian mà tớ ngóng team ra bài hát.. Rồi chờ riết mòn dép luôn. Mong team ra nhiều bài hát mới nhé :)))';
+
+$txt['n42'] = 'Bánh Mì';
+$txt['s42'] = 'Nhạc rất hay rất ấn tượng';
+?>
