ea7e8de34aee2f5c3ce25acf21a9227128593450
V.V.M Team <miro.vvmteam@gmail.com>
2025-12-19 07:10:54 +0700

    Khoi tao nhanh moi

---
+++ old/inc/newestsong.php
@@ -0,0 +1,25 @@
+<?php
+$txt['link2'] = 'https://youtu.be/2SopEKTHZUU';
+$txt['img2'] = 'images/img_hbt.jpg';
+$txt['tittle2'] = 'REBOOT OUR STORY';
+$txt['vtittle2'] = 'Ca khúc chủ đề Hai Bà Trưng';
+$txt['singer2'] = 'với Miku & Rin';
+
+$txt['link3'] = 'https://youtu.be/KUnpnfy53go';
+$txt['img3'] = 'images/img_melan.jpg';
+$txt['tittle3'] = 'Melancholic';
+$txt['vtittle3'] = 'Muộn phiền tương tư';
+$txt['singer3'] = 'với Kagamine Rin';
+
+$txt['link4'] = 'https://youtu.be/3EiPlH2q5-A';
+$txt['img4'] = 'images/img_rs.jpg';
+$txt['tittle4'] = 'Ren\'ai Saiban';
+$txt['vtittle4'] = 'Tòa án tình yêu';
+$txt['singer4'] = 'với Hatsune Miku';
+
+$txt['link1'] = 'https://youtu.be/3IEXsJSALYA';
+$txt['img1'] = 'images/6tr.jpg';
+$txt['tittle1'] = 'Roku-chou Nen to Ichiya...';
+$txt['vtittle1'] = 'Câu chuyện sáu nghìn tỷ năm và một đêm';
+$txt['singer1'] = 'với IA';
+?>
---
+++ old/mtgame.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "./inc/headtag.php" ?>
+  <body>
+  <?php include "./inc/header.php" ?>
+    
+  
+ 
+
+  <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url('images/bg_mt.jpg');"
+    data-aos="fade" data-stellar-background-ratio="0.5" data-aos="fade">
+    <div class="container">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-md-7 text-center" data-aos="fade-up" data-aos-delay="400">
+          <h1 class="text-white">M.T Game</h1>
+          <p>Những sản phẩm game được Việt hóa bởi M.T Game trực thuộc V.V.M Team.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="site-section">
+      <div class="container">
+        <div class="d-block d-md-flex podcast-entry bg-white2 mb-5" data-aos="fade-up">
+          <div class="image" style="background-image: url('images/game/detention.jpg');"></div>
+          <div class="text">
+            <h3 class="font-weight-light"><a href="https://detention.vvmteam.xyz/" target=_blank>返校Detention - Giải thoát</a></h3>
+            <div class=" mb-3 text-black"><span class="text-black-opacity-05"><small>返校Detention <span class="sep">/</span> Hệ điều hành: Windows, Android</small></span></div>
+            <p class="mb-4 text-black">Detention là trò chơi điện tử thuộc thể loại kinh dị sinh tồn mạo hiểm được phát triển bởi nhóm sản xuất trò chơi Đài Loan Red Candle Games cho nền tảng Steam. Trò chơi làm bằng đồ họa 2D, với không khí kinh dị trong bối cảnh Đài Loan dưới chế độ Thiết quân luật những năm 1960.
+<br><br>● Dịch thuật: Miro-chan V.V.M Team<br>
+● Dịch thơ: Cá V.V.M Team, Quỳnh V.V.M Team<br>
+● Hiệu chỉnh nội dung: Bê, Kazuya
+<br>● Link tải: <a href="https://www.youtube.com/watch?v=KvXZG9TV6vQ" target=_blank>Phần mô tả Youtube</a><br> 
+			</p>
+          </div>
+        </div>
+
+        <div class="d-block d-md-flex podcast-entry bg-white2 mb-5" data-aos="fade-up">
+          <div class="image" style="background-image: url('https://ib.vvmteam.xyz/IbBanner.png');"></div>
+          <div class="text">
+            <h3 class="font-weight-light"><a href="https://ib.vvmteam.xyz/" target=_blank>Ib</a></h3>
+            <div class=" mb-3 text-black"><span class="text-black-opacity-05"><small>Ib <span class="sep">/</span> Hệ điều hành: Windows</small></span></div>
+            <p class="mb-4 text-black">Một cô bé tên là Ib đến tham quan một buổi triễn lãm nghệ thuật cùng với bố mẹ.<br>
+Trong khi mãi quan sát những tác phẩm, cô bé đột nhiên nhận ra cô chỉ có một mình.<br>
+Và trong khi cô bé tìm kiếm những người khác, cô tìm thấy những thứ kì lạ trong buổi triển lãm...
+<br><br>● Rinta: Dịch thuật, Hình ảnh<br>
+● Miro: Biên dịch, Hiệu chỉnh, Kĩ thuật, Hình ảnh
+<br>● Link tải: <a href="https://www.youtube.com/watch?v=ZAD6jLBo694" target=_blank>Phần mô tả Youtube</a><br> 
+			</p>
+          </div>
+        </div>
+
+        <div class="d-block d-md-flex podcast-entry bg-white2 mb-5" data-aos="fade-up">
+          <div class="image" style="background-image: url('images/game/pvz.jpg');"></div>
+          <div class="text">
+            <h3 class="font-weight-light"><a href="https://www.youtube.com/watch?v=DeuUIdJifC4" target=_blank>Hoa quả nổi giận</a></h3>
+            <div class=" mb-3 text-black"><span class="text-black-opacity-05"><small>Plants vs. Zombies <span class="sep">/</span> Hệ điều hành: Windows</small></span></div>
+            <p class="mb-4 text-black">Plants vs Zombies là một trò chơi điện tử dạng phòng thủ tháp được PopCap Games phát triển và phát hành. Game lấy bối cảnh của một thành phố bị các thây ma (zombies) tấn công và các thây ma này đã tiến đến ngôi nhà và ăn não của người chơi. Nhiệm vụ của người chơi là phải trồng các loài thực vật đặc biệt để tiêu diệt các thây ma này. Game có nhiều chế độ cùng màn chơi khác nhau cho người chơi khám phá cũng như thư giãn.
+<br><br>● Dịch thuật: Miro, Aiko<br>
+● Hình ảnh: Miro, Rinta<br>
+● Kĩ thuật & Biên tập: Miro<br>
+● Music Video (Dịch thuật): Miro, Quỳnh
+<br>● Link tải: <a href="https://www.youtube.com/watch?v=DeuUIdJifC4" target=_blank>Phần mô tả Youtube</a><br> 
+			</p>
+          </div>
+        </div>
+
+        <div class="d-block d-md-flex podcast-entry bg-white2 mb-5" data-aos="fade-up">
+          <div class="image" style="background-image: url('images/game/devotion.jpg');"></div>
+          <div class="text">
+            <h3 class="font-weight-light"><a href="https://www.youtube.com/watch?v=M_HSRCulqSo" target=_blank>Hoàn Nguyện;Thành Tâm</a></h3>
+            <div class=" mb-3 text-black"><span class="text-black-opacity-05"><small>還願Devotion <span class="sep">/</span> Hệ điều hành: Windows</small></span></div>
+            <p class="mb-4 text-black">Devotion PC được phát triển theo phong cách phiêu lưu kinh dị với góc nhìn người thứ nhất. Game mô phỏng cuộc sống của 1 gia đình tại Đài Loan với bầu không khí u ám đáng sợ, nơi bị che mờ bởi niềm tin tôn giáo dị biệt. Khám phá khu chung cư Đài Loan những năm 1980 - 1 khu nhà cũ kỹ chứa đựng nhiều điều đáng sợ và trở thành nỗi ám ảnh kinh hoàng với bất kỳ ai đặt chân đến. Đi sâu vào lời thề của mỗi thành viên trong gia và chứng kiến sự tận tâm của họ.
+<br><br>● Dịch thuật: Miro, Rinta<br>
+● Hỗ trợ: Apple119<br>
+● Kỹ thuật: Miro
+<br>● Link tải: <a href="https://www.youtube.com/watch?v=M_HSRCulqSo" target=_blank>Phần mô tả Youtube</a><br> 
+			</p>
+          </div>
+        </div>
+
+        <div class="d-block d-md-flex podcast-entry bg-white2 mb-5" data-aos="fade-up">
+          <div class="image" style="background-image: url('images/game/60s.jpg');"></div>
+          <div class="text">
+            <h3 class="font-weight-light"><a href="https://www.youtube.com/watch?v=avkvwhN2A7s" target=_blank>60 Giây!</a></h3>
+            <div class=" mb-3 text-black"><span class="text-black-opacity-05"><small>60 Seconds! <span class="sep">/</span> Hệ điều hành: Windows, Android, iOS</small></span></div>
+            <p class="mb-4 text-black">Trong 60 Giây! bạn sẽ vào vai Ted - một ông bố cùng người vợ và hai đứa con phải đối mặt với thảm họa hạt nhân.
+<br><br>● Dịch thuật: Miro, Rinta, Aiko<br>
+● Hỗ trợ: Cua<br>
+● Kỹ thuật: Miro<br>
+● Kỹ thuật Android: _Cloud_
+<br>● Link tải: <a href="https://www.youtube.com/watch?v=avkvwhN2A7s" target=_blank>Phần mô tả Youtube</a><br> 
+			</p>
+          </div>
+        </div>
+
+        <div class="d-block d-md-flex podcast-entry bg-white2 mb-5" data-aos="fade-up">
+          <div class="image" style="background-image: url('images/game/cuphead.jpg');"></div>
+          <div class="text">
+            <h3 class="font-weight-light"><a href="https://www.youtube.com/watch?v=aUSCK0T8KwU" target=_blank>Cuphead "Đừng giao kèo với Ác quỷ"</a></h3>
+            <div class=" mb-3 text-black"><span class="text-black-opacity-05"><small>Cuphead <span class="sep">/</span> Hệ điều hành: Windows</small></span></div>
+            <p class="mb-4 text-black">"Ngày xửa ngày xưa, ở một nơi huyền diệu tên là Đảo Lọ Mực, có hai anh em tên là Cuphead và Mugman. Họ sống thoải mái dưới đôi mắt tinh tường của Ấm đun Già cỗi thông thái. Một ngày nọ, hai cậu bé lang thang xa khỏi ngôi nhà -- bất chấp việc Ấm đun Già cỗi đã nhiều lần cảnh báo -- và dừng chân tại Sòng Bài Ác Quỷ..."
+<br><br>● Dịch thuật: Miro<br>
+● Kỹ thuật: _Cloud_, Miro<br>
+● Hỗ trợ: RinTa
+<br>● Link tải: <a href="https://www.youtube.com/watch?v=aUSCK0T8KwU" target=_blank>Phần mô tả Youtube</a><br> 
+			</p>
+          </div>
+        </div>
+
+        <div class="d-block d-md-flex podcast-entry bg-white2 mb-5" data-aos="fade-up">
+          <div class="image" style="background-image: url('images/game/fol.jpg');"></div>
+          <div class="text">
+            <h3 class="font-weight-light"><a href="https://www.facebook.com/v.v.m.team/photos/a.701865129856518/2372107532832261/" target=_blank>Nắm đấm tình yêu (18+)</a></h3>
+            <div class=" mb-3 text-black"><span class="text-black-opacity-05"><small>Fist of Love (18+) <span class="sep">/</span> Hệ điều hành: Windows</small></span></div>
+            <p class="mb-4 text-black">Trò chơi này đơn giản chỉ là trò kéo búa bao lột dồ thôi, hông có gì to tát đâu nhe :v
+<br><br>● Việt hóa bởi V.V.M Team
+<br>● Link tải: <a href="https://www.facebook.com/v.v.m.team/photos/a.701865129856518/2372107532832261/" target=_blank>tại đây</a><br> 
+			</p>
+          </div>
+        </div>
+
+        <div class="d-block d-md-flex podcast-entry bg-white2 mb-5" data-aos="fade-up">
+          <div class="image" style="background-image: url('images/game/mm.jpg');"></div>
+          <div class="text">
+            <h3 class="font-weight-light"><a href="https://www.facebook.com/v.v.m.team/photos/a.701865129856518/2415991611777186/" target=_blank>bỏ lỡ tin nhắn.</a></h3>
+            <div class=" mb-3 text-black"><span class="text-black-opacity-05"><small>missed messages. <span class="sep">/</span> Hệ điều hành: Windows</small></span></div>
+            <p class="mb-4 text-black">"iPhone của goth gf" đã gửi cho bạn một bức ảnh.<br>
+Chấp nhận hay từ chối?<br>
+Một câu chuyện lãng mạn / kinh dị về cuộc sống, cái chết và meme.<br>
+Một chút thay đổi về quan điểm có thể thay đổi được mọi thứ.<br>
+Cảnh báo: trò chơi có chứa nội dung liên quan đến tự sát và tự tổn thương bản thân.
+<br><br>● Dịch thuật: RinTa, Miro-chan<br>
+● Kĩ thuật: _Cloud_, Miro-chan<br>
+● Hình ảnh: RinTa
+<br>● Link tải: <a href="https://www.facebook.com/v.v.m.team/photos/a.701865129856518/2415991611777186/" target=_blank>tại đây</a><br> 
+			</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <?php include "./inc/footer.php" ?>
+
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/mediaelement-and-player.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
